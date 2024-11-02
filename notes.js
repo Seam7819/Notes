9a473ce7741045d9f4f1bbe4f1cb60d80008e8ef
Snu Seam <siamahme766@gmail.com>
2024-11-02 17:04:14 +0600

    commiting

--- notes.js
+++ notes.js
@@ -2,12 +2,32 @@ const handleBtn = () => {
     const inputField = document.getElementById('input-field');
     const getValue = inputField.value;
     inputField.value = '';
-    const displayContainer = document.getElementById('display-container');
-    const div = document.createElement('li');
-    div.innerText = `
-    ${getValue}`
-    displayContainer.appendChild(div);
+    displayNote(getValue);
+    saveToLocalStorage(getValue);
 }
 
+const displayNote = (getValue) => {
+    const displayContainer = document.getElementById('display-container');
+    const li = document.createElement('li');
+    li.innerText = getValue;
+    li.style.backgroundColor = '#660a27'
+    li.style.margin ='10px'
+    li.style.padding ='5px'
+    displayContainer.appendChild(li);
+}
 
+const saveToLocalStorage = (getValue) => {
+    let notes = JSON.parse(localStorage.getItem('notes')) || [];
+    notes.push(getValue);
+    localStorage.setItem('notes', JSON.stringify(notes));
+}
 
+const displayFromLocalStorage = () => {
+    const notes = JSON.parse(localStorage.getItem('notes')) || [];
+    notes.forEach(note => displayNote(note));
+}
+
+// Display stored notes on page load
+window.onload = () => {
+    displayFromLocalStorage();
+}
